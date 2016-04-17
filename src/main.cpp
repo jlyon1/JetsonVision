43d7872446a2ce660c9448dc0ca441d701c03751
jlyon1 <joeylyon1@gmail.com>
2016-04-17 19:52:59 -0400

    updated and fixed frame stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 using namespace std;
 
 int main(int, char**){
   VideoCapture cap; // Need to check the address
-  cap.open("http://10.30.44.20/axis-cgi/mjpeg/video.cgi?asdf.mjpeg");
+
   Mat frame;
   namedWindow("Frame",1);
-  while(true){
-    cap >> frame;
-    if(frame.width > 0){
-      imshow("Frame", frame);
-    }
-
+  frame = imread("pic1.png");
+  if(frame.width > 0){
+    imshow("Frame", frame);
   }
+
   return 0;
 
 }
