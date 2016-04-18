8fbd78a1146d17d0225deddc0b81c46b0587a43a
jlyon1 <joeylyon1@gmail.com>
2016-04-18 08:19:43 -0400

    Begin adding buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <pthread.h>
 
 using namespace cv;
 using namespace std;
+
+bool running = false;
+Mat imgArray [3] = {};
+
+void *runBuffer(VideoCapture c){
+  while(running){
+
+  }
+}
 
 int main(int, char**){
   VideoCapture cap; // Need to check the address
